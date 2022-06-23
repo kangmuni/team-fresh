d9d9f1498b6e9eed6f0f4732e09bfaa93c5b12d9
kangmuni <kangmoonee@gmail.com>
2022-06-24 00:25:04 +0900

    Add main_link component

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import MainHeader from './components/main_header/main_header';
 import MainBusiness from './components/main_business/main_business';
 import MainService from './components/main_service/main_service';
 import MainPoint from './components/main_point/main_point';
+import MainLink from './components/main_link/main_link';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <MainBusiness />
       <MainService />
       <MainPoint />
+      <MainLink />
     </>
   );
 }
---
+++ src/components/main_link/main_link.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Link = styled.section`
+  padding: 130px;
+`;
+
+const Wrapper = styled.div`
+  max-width: 1280px;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+const Contents = styled.div`
+  position: relative;
+  display: flex;
+  width: 380px;
+  height: 500px;
+  border-radius: 20px;
+  margin: 20px;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 20px;
+`;
+
+const Description = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  font-size: 50px;
+  color: white;
+  background-color: black;
+  opacity: 0.2;
+  border-radius: 20px;
+  text-align: center;
+  &:hover {
+    background-color: none;
+    opacity: 1;
+  }
+`;
+
+const MainLink = () => {
+  return (
+    <Link>
+      <Wrapper>
+        <Contents>
+          <Img src="/images/main_service_img01.jpeg"></Img>
+          <Description>News</Description>
+        </Contents>
+        <Contents>
+          <Img src="/images/main_service_img02.jpeg"></Img>
+          <Description>Recruit</Description>
+        </Contents>
+        <Contents>
+          <Img src="/images/main_service_img03.jpeg"></Img>
+          <Description>Timf Story</Description>
+        </Contents>
+      </Wrapper>
+    </Link>
+  );
+};
+
+export default MainLink;
